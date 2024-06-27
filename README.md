3c77a57182a79d439b3128735f7d2c80454e1632
Rodrigo Rodrigues <rdrade121@gmail.com>
2024-06-27 19:40:42 -0300

    Login Interface Done

---
+++ README.md
@@ -0,0 +1,3 @@
+# LoginUnreal
+Simple Login Page on Unreal Engine using C++
+![image](https://github.com/Buskik/LoginUnreal/assets/73497926/2704ce02-a33b-48ee-a48d-9442ea4ebfad)
---
+++ Source/Study/Public/LoginManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LoginManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STUDY_API ULoginManager : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
